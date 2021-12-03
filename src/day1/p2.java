783e7fa2b0aa7a1133d09eefa99d49765bf92a7d
khb8668 <khb8668@g.rit.edu>
2021-12-03 18:25:20 -0500

    day 1 part 2

---
+++ src/day1/p2.java
@@ -0,0 +1,34 @@
+package day1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class p2 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        int window1 = 0;
+        int window2 = 0;
+        int increases = 0;
+
+        File file = new File("data");
+        Scanner scan = new Scanner(file);
+
+        ArrayList<Integer> depths = new ArrayList<Integer>();
+        while (scan.hasNextInt()) {
+            depths.add(scan.nextInt());
+        }
+
+        for (int i = 3; i < 2000; i++) {
+            window1 = depths.get(i - 1) + depths.get(i - 2) + depths.get(i - 3);
+            window2 = depths.get(i) + depths.get(i - 1) + depths.get(i - 2);
+
+            if (window2 > window1) {
+                increases++;
+            }
+        }
+        System.out.println(increases);
+    }
+}
+
