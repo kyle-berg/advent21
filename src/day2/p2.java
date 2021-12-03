8538e30757ddd21ad63a1004dce80559856113c7
khb8668 <khb8668@g.rit.edu>
2021-12-03 18:42:44 -0500

    day 2 part 2

---
+++ src/day2/p2.java
@@ -0,0 +1,35 @@
+package day2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class p2 {
+    public static void main(String[] args) throws FileNotFoundException {
+        int horiz = 0;
+        int depth = 0;
+        int aim = 0;
+        File file = new File("data");
+        Scanner scan = new Scanner(file);
+
+        while (scan.hasNext()) {
+            String str = scan.nextLine();
+            String[] command = str.split(" ");
+            if (command[0].equals("forward")) {
+                horiz += Integer.parseInt(command[1]);
+                depth += aim * Integer.parseInt(command[1]);
+            }
+            else if (command[0].equals("down")) {
+                //depth += Integer.parseInt(command[1]);
+                aim += Integer.parseInt(command[1]);
+            }
+            else {
+                //depth -= Integer.parseInt(command[1]);
+                aim -= Integer.parseInt(command[1]);
+            }
+        }
+        System.out.println("Horizontal: " + horiz);
+        System.out.println("Depth: " + depth);
+        System.out.println(horiz*depth);
+    }
+}
