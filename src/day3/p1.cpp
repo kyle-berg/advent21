29afb10b3ea1ef36eab2677f82e061fac3ebdf90
khb8668 <khb8668@g.rit.edu>
2021-12-05 21:54:36 -0500

    day 3 and day 4 initialization

---
+++ src/day3/p1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ifstream input{"data.txt"};
+
+    vector<int> zeroes;
+    vector<int> ones;
+
+    string line;
+    //while(input >> line);
+}
